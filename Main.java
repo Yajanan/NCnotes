34d0a33df1b089ccc2165893b38e09fe349d2ee6
yajanan <yajanan@163.com>
2020-10-06 22:02:00 +0800

    add displaynote function

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+
+    static void displayNote(String content){
+        System.out.println(content);
+    }
+
+    static String getFileContent(File f){
+        try (FileReader fr = new FileReader(f)){
+            char[] content = new char[(int)f.length()];
+            int readLength = fr.read(content);
+            if(readLength >= 0){
+                return new String(content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        File f = new File("./a.txt");
+        String content = getFileContent(f);
+        displayNote(content);
     }
 }
