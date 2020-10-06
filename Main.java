0cbeb218b85683b4e99c262283387d31078fafbd
yajanan <yajanan@163.com>
2020-10-06 22:33:07 +0800

    add functions,new file and show all notes

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+    
+    static Scanner s = new Scanner(System.in);
 
     static void displayNote(String content){
         System.out.println(content);
@@ -21,9 +25,58 @@ public class Main {
         return null;
     }
 
+    static void newFile(){
+        System.out.println("Please type the filename:");
+        String filename = s.next();
+        
+        System.out.println("Please type the content:");
+        String content = s.next();
+
+        File f = new File("./notes/" + filename);
+
+        writeFile(f, content);
+    }
+
+    static void writeFile(File f, String content){
+        try(FileWriter fw = new FileWriter(f)){
+            char[] cs = content.toCharArray();
+            fw.write(cs);
+            fw.flush();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    static void showPath(){
+        File f = new File("./notes/");
+        String[] notesList = f.list();
+        if(notesList != null){
+            for(String note : notesList){
+                System.out.println(note);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        File f = new File("./a.txt");
-        String content = getFileContent(f);
-        displayNote(content);
+        System.out.println("Welcome to NCnotes!");
+        System.out.println("Please type what you want to do:");
+        System.out.println("N/n:new S/s:show path");
+
+        String commend = s.next();
+
+        switch(commend){
+            case "N":
+            case "n":
+                newFile();
+                break;
+            case "S":
+            case "s":
+                showPath();
+                break;
+            default:
+                break;
+        }
+        
+        s.close();
     }
 }
